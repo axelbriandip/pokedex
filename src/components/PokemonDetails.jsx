b7b5fdff4df1869099ca826dfe249152cc71017f
axelbriandip <axelbriandip.rg@gmail.com>
2022-07-26 05:03:57 -0300

    hero from details done

--- src/components/PokemonDetails.jsx
+++ src/components/PokemonDetails.jsx
@@ -10,10 +10,36 @@ const PokemonDetails = () => {
             .then(res => setPokemon(res.data))
             .catch(err => console.log(err))
     }, [ id ])
-    console.log(`${pokemon.name} - ${pokemon.id}`);
     return (
-        <div>
-            <h1>test pokemon details: {id}</h1>
+        <div className='container-pokemon-details'>
+            <div className="header">
+                <img src="../img/logo-pokemon.png"/>
+            </div>
+            <div className="grid-container">
+                <div className="hero-detail">
+                    <div className='hero-first'>
+                        <div>
+                            <span>{pokemon.weight}</span>
+                            <span>Weight</span>
+                        </div>
+                        <div>
+                            <img src={pokemon.sprites?.front_default}/>
+                        </div>
+                        <div>
+                            <span>{pokemon.height}</span>
+                            <span>Height</span>
+                        </div>
+                    </div>
+                    <div className='hero-second'>
+                        <h1>{pokemon.name}</h1>
+                        <span># {pokemon.id}</span>
+                    </div>
+                </div>
+                <div className="movements">mov</div>
+                <div className="type">type</div>
+                <div className="habilities">habilities</div>
+                <div className="stats">stats</div>
+            </div>
         </div>
     );
 };
